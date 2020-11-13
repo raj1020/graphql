0b246d4efb66eea30fd810948a752530bc846ce8
raj1020 <raj.1020@hotmail.com>
2020-11-14 09:55:04 +1100

    added graphqlschema and rootquery

--- server/app.js
+++ server/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const grapqlHTTP = require('express-graphql');
+const schema = require('./schema/schema');
 
 
 
 const app = express();
+
+app.use('/graphql', grapqlHTTP.graphqlHTTP({
+    schema
+}));
 
 app.get('/', (req, res) => {
     res.send("Hello Mailo! How you doin?");
