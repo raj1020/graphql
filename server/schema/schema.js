63527be1c9f8e428795fd17ac6b42cd0b6e94bef
raj1020 <raj.1020@hotmail.com>
2020-11-19 14:06:06 +1100

    defined the mutation for adding authors and adding books

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,7 @@
 const graphql = require('graphql');
-const _ = require('lodash');
 const Book = require('../models/book');
 const Author = require('../models/author');
+const _ = require('lodash');
 
 
 const {
@@ -86,8 +86,46 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
+
+const Mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addAuthor: {
+            type: AuthorType,
+            args: {
+                name: {type: GraphQLString},
+                age: {type: GraphQLInt}
+            },
+            resolve(parent, args) {
+                let author = new Author({
+                    name: args.name,
+                    age: args.age
+                });
+               return author.save();
+            } 
+        },
+        addBook: {
+            type: BookType,
+            args: {
+                name: {type: GraphQLString},
+                genre: {type: GraphQLString},
+                authorId: {type: GraphQLID}
+            },
+            resolve(parent, args){
+                let book = new Book({
+                    name: args.name,
+                    genre: args.genre,
+                    authorId: args.authorId
+                });
+                return book.save();
+            }
+        }
+    }
+})
+
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation: Mutation
 });
 
 
